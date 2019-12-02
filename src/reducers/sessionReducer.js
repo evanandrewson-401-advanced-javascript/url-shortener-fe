5d922b36c8b0aa5e47043b6f2f6a077222839f19
evanandrewson <evanandrewson@gmail.com>
2019-12-02 09:41:11 -0800

    config, set up store, app.js

---
+++ src/reducers/sessionReducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+  username: null,
+  _id: null,
+  loading: true,
+  error: null
+};
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
